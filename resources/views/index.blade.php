53fa234d3656a127114252d8e5c1c150c0f4574a
Yessoufou Yasmine <83721329+YasmineYH@users.noreply.github.com>
2022-02-16 02:05:12 +0100

    Update database connection

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
-    <title>Unilorin VLE</title>
-</head>
-<body>
+@extends('layouts.vle_home')
+
+@section('page-content')
     <section class="home home-index">
         <img src="{{ asset('images/logo.png') }}" alt="Go">
         <h1>University of Ilorin Virtual Learning Environment</h1>
@@ -18,5 +11,4 @@
         </div>
     </section>
     <script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
+@endsection
