c1531aa48c669d4729d125bad442f18c5121f36c
Yessoufou Yasmine <83721329+YasmineYH@users.noreply.github.com>
2022-02-20 20:16:36 +0100

    d

--- resources/views/lecturer/message.blade.php
+++ resources/views/lecturer/message.blade.php
@@ -4,8 +4,29 @@
     <div class="modal message" style="display: grid; width: 100%; left: 0; height: calc(var(--vh) * 100)">
         <div class="modal-box">
             <div class="modal-content modal-content-confirm">
-                <h3 style="margin-top: -10px; margin-bottom: 40px; text-align: center">First step completed! Click OK to move to the next steps.</h3>
-                <button>Styles</button>
+                @if ($success == 'step 1')
+                    <h3 style="margin-top: -10px; margin-bottom: 40px; text-align: center">First step completed! Click OK to move to the next steps.</h3>
+                    @if ($type == 'Theory')
+                        <a href="{{ route('add_assignment_step2_theory', ['assignID' => $assignID, 'courseCode' => $courseCode, 'lecturerData' => $lecturerData]) }}" class="normal-btn">OK</a>
+                    @endif
+
+                    @if ($type == 'Essay')
+                        <a href="{{ route('add_assignment_step2_essay', ['assignID' => $assignID, 'courseCode' => $courseCode, 'lecturerData' => $lecturerData]) }}" class="normal-btn">OK</a>
+                    @endif
+
+                    @if ($type == 'Multiple Choice')
+                        <a href="{{ route('add_assignment_step2_obj', ['assignID' => $assignID, 'courseCode' => $courseCode, 'lecturerData' => $lecturerData]) }}" class="normal-btn">OK</a>
+                    @endif
+
+                    @if ($type == 'Subjective')
+                        <a href="{{ route('add_assignment_step2_subj', ['assignID' => $assignID, 'courseCode' => $courseCode, 'lecturerData' => $lecturerData]) }}" class="normal-btn">OK</a>
+                    @endif
+                @endif
+
+                @if ($success == 'step 2')
+                    <h3 style="margin-top: -10px; margin-bottom: 40px">Assignment addition complete!</h3>
+                    <a href="{{ route('lecturer_assignments_d', ['courseCode' => $courseCode, 'lecturerData' => $lecturerData]) }}" class="normal-btn">End</a>
+                @endif
             </div>
         </div>
     </div>
