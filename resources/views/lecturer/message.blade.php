3b85a82712dc291a66330315f98dcefe0f3b9678
Yessoufou Yasmine <83721329+YasmineYH@users.noreply.github.com>
2022-02-20 20:02:19 +0100

    d

--- resources/views/lecturer/message.blade.php
+++ resources/views/lecturer/message.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.message')
 
 @section('page-content')
-    <div class="modal message" style="visibility: visible; width: 100%; left: 0; height: calc(var(--vh) * 100)">
+    <div class="modal message">
         <div class="modal-box">
             <div class="modal-content modal-content-confirm">
-                <h3 style="margin-top: -10px; margin-bottom: 40px; text-align: center">First step completed! Click OK to move to the next steps.</h3>
+                <h3>First step completed! Click OK to move to the next steps.</h3>
                 <button>Styles</button>
             </div>
         </div>
