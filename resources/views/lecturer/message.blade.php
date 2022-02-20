d8011e3367c17c538897ae2b5cebe25d9619ac5f
Yessoufou Yasmine <83721329+YasmineYH@users.noreply.github.com>
2022-02-20 19:06:54 +0100

    d

--- resources/views/lecturer/message.blade.php
+++ resources/views/lecturer/message.blade.php
@@ -1,22 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/app.css" />
-    <title>Message</title>
-</head>
-<body>
-    <main>
-        <div class="modal message" style="visibility: visible; width: 100%; left: 0; height: calc(var(--vh) * 100)">
-            <div class="modal-box">
-                <div class="modal-content modal-content-confirm">
-                    <h3 style="margin-top: -10px; margin-bottom: 40px; text-align: center">First step completed! Click OK to move to the next steps.</h3>
-                    
-                </div>
+@extends('layouts.message')
+
+@section('page-content')
+    <div class="modal message" style="visibility: visible; width: 100%; left: 0; height: calc(var(--vh) * 100)">
+        <div class="modal-box">
+            <div class="modal-content modal-content-confirm">
+                <h3 style="margin-top: -10px; margin-bottom: 40px; text-align: center">First step completed! Click OK to move to the next steps.</h3>
+                
             </div>
         </div>
-    </main>
-</body>
-</html>
+    </div>
+@endsection
