7776529a14d2a179bc0715a8bde74ef3068aa306
Yessoufou Yasmine <83721329+YasmineYH@users.noreply.github.com>
2022-02-20 02:17:02 +0100

    d

--- resources/views/lecturer/courses.blade.php
+++ resources/views/lecturer/courses.blade.php
@@ -8,14 +8,14 @@
         @foreach ($lecturerCourses as $lecturerCourse)
             <li class="normal-card menu">
                 <div class="card-a">
-                    <h3>{{ $lecturerCourse->CourseCode }}</h3>
+                    <h3>{{ $lecturerCourse->coursecode }}</h3>
                 </div>
                 <div class="floating-menu">
                     <div class="floating-menu-shadow"></div>
                     <ul>
-                        <li><a href="{{ Route('lecturer_students', ['courseCode' => $lecturerCourse->CourseCode]) }}"><h3>{{ $lecturerCourse->CourseCode }} Students</h3></a></li>
-                        <li><a href="{{ Route('lecturer_library_cm', ['courseCode' => $lecturerCourse->CourseCode]) }}"><h3>{{ $lecturerCourse->CourseCode }} Library</h3></a></li>
-                        <li><a href="{{ Route('lecturer_assignments_d', ['courseCode' => $lecturerCourse->CourseCode]) }}"><h3>{{ $lecturerCourse->CourseCode }} Assignments</h3></a></li>
+                        <li><a href="{{ Route('lecturer_students', ['courseCode' => $lecturerCourse->coursecode]) }}"><h3>{{ $lecturerCourse->coursecode }} Students</h3></a></li>
+                        <li><a href="{{ Route('lecturer_library_cm', ['courseCode' => $lecturerCourse->coursecode]) }}"><h3>{{ $lecturerCourse->coursecode }} Library</h3></a></li>
+                        <li><a href="{{ Route('lecturer_assignments_d', ['courseCode' => $lecturerCourse->coursecode]) }}"><h3>{{ $lecturerCourse->coursecode }} Assignments</h3></a></li>
                     </ul>
                 </div>
             </li>
